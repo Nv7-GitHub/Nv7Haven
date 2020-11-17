0403b051a16f9758564af0fa6414027f36f2897f
Nv7 <junk4Nv7@gmail.com>
2020-11-17 11:08:02 -0800

    Nicer demo

--- main.go
+++ main.go
@@ -10,7 +10,11 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendString("hmmmm looks like nothing is here yet... Maybe check out /ping?")
+	})
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		return c.SendString("pong")
 	})
 
 	log.Fatal(app.Listen(":8080"))
