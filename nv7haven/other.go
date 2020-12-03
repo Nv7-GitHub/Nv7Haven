14aa93133588790059000e7f14c00fbe5e2f3dad
Nv7 <junk4Nv7@gmail.com>
2020-12-03 13:09:52 -0800

    get rid of forwarding checking since in fiber

--- nv7haven/other.go
+++ nv7haven/other.go
@@ -3,9 +3,5 @@ package nv7haven
 import "github.com/gofiber/fiber/v2"
 
 func getIP(c *fiber.Ctx) error {
-	forwarded := c.Get("X-FORWARDED-FOR")
-	if forwarded != "" {
-		return c.SendString(forwarded)
-	}
 	return c.SendString(c.IP())
 }
