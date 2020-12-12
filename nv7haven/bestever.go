3505d26d8af56278499bd5cfc47361dcd6ffeee7
Nv7 <junk4Nv7@gmail.com>
2020-12-12 11:52:41 -0800

    Get rid of print

--- nv7haven/bestever.go
+++ nv7haven/bestever.go
@@ -43,7 +43,6 @@ func (n *Nv7Haven) initBestEver() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(rawData))
 	var rawMarshaled map[string][]Suggestion
 	err = json.Unmarshal(rawData, &rawMarshaled)
 	if err != nil {
