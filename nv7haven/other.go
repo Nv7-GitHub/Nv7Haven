032a04725cd92b47c87a06eebf569594814ce51b
Nv7 <junk4Nv7@gmail.com>
2020-12-13 16:18:49 -0800

    Change IP func

--- nv7haven/other.go
+++ nv7haven/other.go
@@ -1,12 +1,9 @@
 package nv7haven
 
 import (
-	"net"
-	"strings"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func (d *Nv7Haven) getIP(c *fiber.Ctx) error {
-	return c.SendString(net.ParseIP(strings.Split(c.Get("X-Forwarded-For"), ",")[0]).String())
+	return c.SendString(c.IP())
 }
