d582c0d2291bfdac4aa13401c2cc906f36363456
Nv7 <junk4Nv7@gmail.com>
2020-12-13 16:50:02 -0800

    Proper headers

--- nv7haven/other.go
+++ nv7haven/other.go
@@ -3,5 +3,7 @@ package nv7haven
 import "github.com/gofiber/fiber/v2"
 
 func (d *Nv7Haven) getIP(c *fiber.Ctx) error {
+	c.Set("Access-Control-Allow-Origin", "*")
+	c.Set("Access-Control-Allow-Headers", "*")
 	return c.SendString(c.IPs()[0])
 }
