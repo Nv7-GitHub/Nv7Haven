f96187d2e72de28af0c2442c69365dc000046eb9
Nv7 <junk4Nv7@gmail.com>
2020-12-13 20:03:15 -0800

    Change routing

--- nv7haven/nv7haven.go
+++ nv7haven/nv7haven.go
@@ -29,6 +29,7 @@ func (c *Nv7Haven) routing(app *fiber.App) {
 	app.Get("/new_note/:name/", c.newNote)
 	app.Get("/change_note/:name/:password", c.changeNote)
 	app.Get("/get_note/:name/:password", c.getNote)
+	app.Get("/get_note/:name", c.getNote)
 	app.Get("/has_password/:name", c.hasPassword)
 	app.Get("/note_search/:query", c.searchNotes)
 }
