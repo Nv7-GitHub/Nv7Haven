27bec7af7f616e7da5cb854081f63e8e293558be
Nv7 <junk4Nv7@gmail.com>
2020-12-13 16:49:09 -0800

    Change import

--- nv7haven/other.go
+++ nv7haven/other.go
@@ -1,8 +1,6 @@
 package nv7haven
 
-import (
-	"github.com/gofiber/fiber/v2"
-)
+import "github.com/gofiber/fiber/v2"
 
 func (d *Nv7Haven) getIP(c *fiber.Ctx) error {
 	return c.SendString(c.IPs()[0])
