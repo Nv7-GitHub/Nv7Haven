8bb036e530c3df803190f040047b3d4df673f222
Nv7 <junk4Nv7@gmail.com>
2020-12-14 18:47:10 -0800

    Giveum commnad

--- discord/givenum.go
+++ discord/givenum.go
@@ -2,6 +2,7 @@ package discord
 
 import (
 	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -12,6 +13,42 @@ func (b *Bot) giveNum(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
+	// givenum command
+	if strings.HasPrefix(m.Content, "givenum") {
+		var num int
+		_, err := fmt.Sscanf(m.Content, "givenum %d", &num)
+		if b.handle(err, m) {
+			return
+		}
+		res, err := b.db.Query("SELECT COUNT(1) FROM givenum WHERE guild=? AND member=? LIMIT 1", m.GuildID, m.Author.ID)
+		defer res.Close()
+		if b.handle(err, m) {
+			return
+		}
+		var count int
+		if (count > 100) || (count < 0) {
+			s.ChannelMessageSend(m.ChannelID, "You need to choose a number from 0-100.")
+			return
+		}
+		res.Next()
+		res.Scan(&count)
+		if count == 0 {
+			_, err = b.db.Exec("INSERT INTO givenum VALUES ( ?, ?, ? )", m.GuildID, m.Author.ID, num)
+			if b.handle(err, m) {
+				return
+			}
+			s.ChannelMessageSend(m.ChannelID, "Successfully initialized value.")
+			return
+		}
+		_, err = b.db.Exec("UPDATE givenum SET number=? WHERE guild=? AND member=?", num, m.GuildID, m.Author.ID)
+		if b.handle(err, m) {
+			return
+		}
+		s.ChannelMessageSend(m.ChannelID, "Successfully updated value.")
+		return
+	}
+
+	// roles command
 	if strings.HasPrefix(m.Content, "roles") {
 		mem, err := s.GuildMember(m.GuildID, m.Mentions[0].ID)
 		if b.handle(err, m) {
--- discord/main.go
+++ discord/main.go
@@ -1,14 +1,24 @@
 package discord
 
 import (
+	"database/sql"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
+	_ "github.com/go-sql-driver/mysql" // mysql
 )
 
-const token = "Nzg4MTg1MzY1NTMzNTU2NzM2.X9f00g.krA6cjfFWYdzbqOPXq8NvRjxb3k"
+const (
+	dbUser     = "u29_c99qmCcqZ3"
+	dbPassword = "j8@tJ1vv5d@^xMixUqUl+NmA"
+	dbName     = "s29_nv7haven"
+	token      = "Nzg4MTg1MzY1NTMzNTU2NzM2.X9f00g.krA6cjfFWYdzbqOPXq8NvRjxb3k"
+)
 
 // Bot is a discord bot
 type Bot struct {
 	dg *discordgo.Session
+	db *sql.DB
 }
 
 func (b *Bot) handlers() {
@@ -17,12 +27,17 @@ func (b *Bot) handlers() {
 
 // InitDiscord creates a discord bot
 func InitDiscord() Bot {
+	db, err := sql.Open("mysql", dbUser+":"+dbPassword+"@tcp("+os.Getenv("MYSQL_HOST")+":3306)/"+dbName)
+	if err != nil {
+		panic(err)
+	}
 	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		panic(err)
 	}
 	b := Bot{
 		dg: dg,
+		db: db,
 	}
 	b.handlers()
 	dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
@@ -35,7 +50,7 @@ func InitDiscord() Bot {
 
 func (b *Bot) handle(err error, m *discordgo.MessageCreate) bool {
 	if err != nil {
-		b.dg.ChannelMessageSend(m.ChannelID, err.Error())
+		b.dg.ChannelMessageSend(m.ChannelID, "Error: "+err.Error())
 		return true
 	}
 	return false
