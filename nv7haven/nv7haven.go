9691a5ab0792a8c34680108fbf4cd329b9cebc2a
Nv7 <junk4Nv7@gmail.com>
2020-12-17 10:51:32 -0800

    Try SSE

--- nv7haven/nv7haven.go
+++ nv7haven/nv7haven.go
@@ -2,15 +2,13 @@ package nv7haven
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/Nv7-Github/firebase"
 	database "github.com/Nv7-Github/firebase/db"
 	_ "github.com/go-sql-driver/mysql" // mysql
+	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/websocket/v2"
 )
 
 // Nv7Haven is the backend for https://nv7haven.tk
@@ -44,46 +42,8 @@ func (c *Nv7Haven) routing(app *fiber.App) {
 	app.Get("/tf_like/:name", c.like)
 	app.Post("/tf_comment/:name", c.comment)
 	app.Get("/tf_get/:name", c.getPost)
-	app.Use("/ws", func(c *fiber.Ctx) error {
-		// IsWebSocketUpgrade returns true if the client
-		// requested upgrade to the WebSocket protocol.
-		if websocket.IsWebSocketUpgrade(c) {
-			c.Locals("allowed", true)
-			return c.Next()
-		}
-		return fiber.ErrUpgradeRequired
-	})
+	app.Get("/sse/validation", adaptor.HTTPHandler(handler(dashbaordHandler)))
 
-	app.Get("/ws/:id", websocket.New(func(c *websocket.Conn) {
-		// c.Locals is added to the *websocket.Conn
-		log.Println(c.Locals("allowed"))  // true
-		log.Println(c.Params("id"))       // 123
-		log.Println(c.Query("v"))         // 1.0
-		log.Println(c.Cookies("session")) // ""
-
-		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
-		var (
-			mt  int
-			msg []byte
-			err error
-		)
-		for {
-			if mt, msg, err = c.ReadMessage(); err != nil {
-				log.Println("read:", err)
-				break
-			}
-			log.Printf("recv: %s", msg)
-
-			fmt.Print("input?")
-			fmt.Scanln(&msg)
-
-			if err = c.WriteMessage(mt, msg); err != nil {
-				log.Println("write:", err)
-				break
-			}
-		}
-
-	}))
 }
 
 const (
---
+++ nv7haven/test.go
@@ -0,0 +1,42 @@
+package nv7haven
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(f http.HandlerFunc) http.Handler {
+	return http.HandlerFunc(f)
+}
+func dashbaordHandler(w http.ResponseWriter, r *http.Request) {
+	chn := make(chan bool)
+	go func() {
+		time.Sleep(1)
+		chn <- true
+	}()
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+
+	timeout := time.After(1 * time.Second)
+	select {
+	case ev := <-chn:
+		var buf bytes.Buffer
+		enc := json.NewEncoder(&buf)
+		enc.Encode(ev)
+		fmt.Fprintf(w, "data: %v\n\n", buf.String())
+		fmt.Printf("data: %v\n", buf.String())
+	case <-timeout:
+		fmt.Fprintf(w, ": nothing to sent\n\n")
+	}
+
+	if f, ok := w.(http.Flusher); ok {
+		f.Flush()
+	}
+}
