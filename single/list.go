a8d3ba9dce6fed6e38f4cd31503b793a212e5587
Nv7 <junk4Nv7@gmail.com>
2021-01-17 19:09:26 -0800

    List

--- single/list.go
+++ single/list.go
@@ -1,1 +1,42 @@
 package single
+
+import (
+	"errors"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type listItem struct {
+	Title       string
+	Description string
+	UID         string
+	ID          string
+}
+
+func (s *Single) list(c *fiber.Ctx) error {
+	var kinds = map[string]string{
+		"date":  "createdOn DESC",
+		"az":    "title ASC",
+		"likes": "likes DESC",
+		"za":    "title DESC",
+	}
+	kind, exists := kinds[c.Params("kind")]
+	if !exists {
+		return errors.New("invalid kind")
+	}
+	res, err := s.db.Query("SELECT title, description, uid, id FROM single ORDER BY " + kind + " WHERE 1")
+	if err != nil {
+		return err
+	}
+	defer res.Close()
+	var list []listItem
+	for res.Next() {
+		item := listItem{}
+		err = res.Scan(&item.Title, &item.Description, &item.UID, &item.ID)
+		if err != nil {
+			return err
+		}
+		list = append(list, item)
+	}
+	return c.JSON(list)
+}
