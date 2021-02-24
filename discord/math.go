24bc2b4399586eb133b5cf21e0d715f02f7777d4
Nv7 <junk4Nv7@gmail.com>
2021-02-24 11:15:40 -0800

    Try new var system

--- discord/math.go
+++ discord/math.go
@@ -12,7 +12,38 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var varput = regexp.MustCompile(`var (.+)=([0-9.,]+)`)
+var varput = regexp.MustCompile(`var (.+)=(.+)`)
+var functions = map[string]govaluate.ExpressionFunction{
+	"sqrt": newFunc(math.Sqrt),
+	"cos":  newFunc(math.Cos),
+	"tan":  newFunc(math.Tan),
+	"sin":  newFunc(math.Sin),
+	"asin": newFunc(math.Asin),
+	"acos": newFunc(math.Acos),
+	"atan": newFunc(math.Atan),
+	"exp": func(args ...interface{}) (interface{}, error) {
+		if len(args) < 2 {
+			return nil, errors.New("Not enough arguments")
+		}
+		val1, ok := args[0].(float64)
+		if !ok {
+			intval, ok := args[0].(int)
+			if !ok {
+				return nil, errors.New("Argument is not number")
+			}
+			val1 = float64(intval)
+		}
+		val2, ok := args[1].(float64)
+		if !ok {
+			intval, ok := args[1].(int)
+			if !ok {
+				return nil, errors.New("Argument is not number")
+			}
+			val2 = float64(intval)
+		}
+		return math.Pow(val1, val2), nil
+	},
+}
 
 func newFunc(fu func(float64) float64) func(...interface{}) (interface{}, error) {
 	return func(args ...interface{}) (interface{}, error) {
@@ -48,48 +79,24 @@ func (b *Bot) math(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		}
 
+		gexp, err := govaluate.NewEvaluableExpressionWithFunctions(m.Content[1:], functions)
+		if b.handle(err, m) {
+			return
+		}
 		_, exists := b.mathvars[m.GuildID]
 		if !exists {
 			b.mathvars[m.GuildID] = make(map[string]interface{})
 		}
-		b.mathvars[m.GuildID][name] = val
+		result, err := gexp.Evaluate(b.mathvars[m.GuildID])
+		if b.handle(err, m) {
+			return
+		}
+		b.mathvars[m.GuildID][name] = result
 		s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Successfuly set variable %s to %f", name, val))
 		return
 	}
 
 	if strings.HasPrefix(m.Content, "=") {
-		functions := map[string]govaluate.ExpressionFunction{
-			"sqrt": newFunc(math.Sqrt),
-			"cos":  newFunc(math.Cos),
-			"tan":  newFunc(math.Tan),
-			"sin":  newFunc(math.Sin),
-			"asin": newFunc(math.Asin),
-			"acos": newFunc(math.Acos),
-			"atan": newFunc(math.Atan),
-			"exp": func(args ...interface{}) (interface{}, error) {
-				if len(args) < 2 {
-					return nil, errors.New("Not enough arguments")
-				}
-				val1, ok := args[0].(float64)
-				if !ok {
-					intval, ok := args[0].(int)
-					if !ok {
-						return nil, errors.New("Argument is not number")
-					}
-					val1 = float64(intval)
-				}
-				val2, ok := args[1].(float64)
-				if !ok {
-					intval, ok := args[1].(int)
-					if !ok {
-						return nil, errors.New("Argument is not number")
-					}
-					val2 = float64(intval)
-				}
-				return math.Pow(val1, val2), nil
-			},
-		}
-
 		gexp, err := govaluate.NewEvaluableExpressionWithFunctions(m.Content[1:], functions)
 		if b.handle(err, m) {
 			return
