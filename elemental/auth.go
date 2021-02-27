a9f74dd604cdacf60d548070152f95e9e00f9ab3
Nv7 <junk4Nv7@gmail.com>
2021-02-26 16:43:44 -0800

    Try to split user up

--- elemental/auth.go
+++ elemental/auth.go
@@ -7,6 +7,90 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// CreateUserResponse is the response to CreateUser
+type CreateUserResponse struct {
+	Success bool
+	Data    string
+}
+
+// CreateUser creates a user
+func (e *Elemental) CreateUser(name string, password string) CreateUserResponse {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 8)
+	if err != nil {
+		return CreateUserResponse{
+			Success: false,
+			Data:    err.Error(),
+		}
+	}
+
+	var uid string
+	count := 1
+	for count != 0 {
+		uid, err = GenerateRandomStringURLSafe(16)
+		if err != nil {
+			return CreateUserResponse{
+				Success: false,
+				Data:    err.Error(),
+			}
+		}
+
+		// Check if name taken
+		res, err := e.db.Query("SELECT COUNT(1) FROM users WHERE uid=? LIMIT 1", name)
+		if err != nil {
+			return CreateUserResponse{
+				Success: false,
+				Data:    err.Error(),
+			}
+		}
+		defer res.Close()
+		res.Next()
+		err = res.Scan(&count)
+		if err != nil {
+			return CreateUserResponse{
+				Success: false,
+				Data:    err.Error(),
+			}
+		}
+	}
+
+	// Check if name taken
+	res, err := e.db.Query("SELECT COUNT(1) FROM users WHERE name=? LIMIT 1", name)
+	if err != nil {
+		return CreateUserResponse{
+			Success: false,
+			Data:    err.Error(),
+		}
+	}
+	defer res.Close()
+	res.Next()
+	err = res.Scan(&count)
+	if err != nil {
+		return CreateUserResponse{
+			Success: false,
+			Data:    err.Error(),
+		}
+	}
+	if count == 1 {
+		return CreateUserResponse{
+			Success: false,
+			Data:    "Account already exists!",
+		}
+	}
+
+	_, err = e.db.Exec("INSERT INTO users VALUES( ?, ?, ?, ? )", name, uid, string(hashedPassword), `["Air", "Earth", "Fire", "Water"]`)
+	if err != nil {
+		return CreateUserResponse{
+			Success: false,
+			Data:    err.Error(),
+		}
+	}
+
+	return CreateUserResponse{
+		Success: true,
+		Data:    uid,
+	}
+}
+
 func (e *Elemental) createUser(c *fiber.Ctx) error {
 	name, err := url.PathUnescape(c.Params("name"))
 	if err != nil {
@@ -22,76 +106,11 @@ func (e *Elemental) createUser(c *fiber.Ctx) error {
 			"data":    err.Error(),
 		})
 	}
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 8)
-	if err != nil {
-		return c.JSON(map[string]interface{}{
-			"success": false,
-			"data":    err.Error(),
-		})
-	}
 
-	var uid string
-	count := 1
-	for count != 0 {
-		uid, err = GenerateRandomStringURLSafe(16)
-		if err != nil {
-			return c.JSON(map[string]interface{}{
-				"success": false,
-				"data":    err.Error(),
-			})
-		}
-
-		// Check if name taken
-		res, err := e.db.Query("SELECT COUNT(1) FROM users WHERE uid=? LIMIT 1", name)
-		if err != nil {
-			return c.JSON(map[string]interface{}{
-				"success": false,
-				"data":    err.Error(),
-			})
-		}
-		defer res.Close()
-		res.Next()
-		err = res.Scan(&count)
-		if err != nil {
-			return c.JSON(map[string]interface{}{
-				"success": false,
-				"data":    err.Error(),
-			})
-		}
-	}
-
-	// Check if name taken
-	res, err := e.db.Query("SELECT COUNT(1) FROM users WHERE name=? LIMIT 1", name)
-	if err != nil {
-		return err
-	}
-	defer res.Close()
-	res.Next()
-	err = res.Scan(&count)
-	if err != nil {
-		return c.JSON(map[string]interface{}{
-			"success": false,
-			"data":    err.Error(),
-		})
-	}
-	if count == 1 {
-		return c.JSON(map[string]interface{}{
-			"success": false,
-			"data":    "Account already exists!",
-		})
-	}
-
-	_, err = e.db.Exec("INSERT INTO users VALUES( ?, ?, ?, ? )", name, uid, string(hashedPassword), `["Air", "Earth", "Fire", "Water"]`)
-	if err != nil {
-		return c.JSON(map[string]interface{}{
-			"success": false,
-			"data":    err.Error(),
-		})
-	}
-
+	resp := e.CreateUser(name, password)
 	return c.JSON(map[string]interface{}{
-		"success": true,
-		"data":    uid,
+		"success": resp.Success,
+		"data":    resp.Data,
 	})
 }
 
