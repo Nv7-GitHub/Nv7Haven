f1c34b0a9bf7c065de109a64abb41b2a9980f3f5
Nv7 <junk4Nv7@gmail.com>
2021-03-12 09:24:43 -0800

    Change back

--- elemental/create.go
+++ elemental/create.go
@@ -60,7 +60,7 @@ func (e *Elemental) incrementUses(id string) error {
 func (e *Elemental) CreateSuggestion(mark string, pioneer string, elem1 string, elem2 string, id string) (bool, string) {
 	existing, err := e.getSugg(id)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 	if !(existing.Votes >= maxVotes) && (int(time.Now().Weekday()) != anarchyDay) {
 		return false, "This element still needs more votes!"
@@ -69,77 +69,55 @@ func (e *Elemental) CreateSuggestion(mark string, pioneer string, elem1 string, 
 	// Get combos
 	combos, err := e.GetSuggestions(elem1, elem2)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 
 	// Delete hanging elements
 	for _, val := range combos {
 		_, err = e.db.Exec("DELETE FROM suggestions WHERE name=?", val)
 		if err != nil {
-			panic(err)
+			return false, err.Error()
 		}
 	}
 
 	// Delete combos
 	_, err = e.db.Exec("DELETE FROM sugg_combos WHERE (elem1=? AND elem2=?) OR (elem1=? AND elem2=?)", elem1, elem2, elem2, elem1)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
-
-	// New Recent Combo
-	go func() {
-		var recents []RecentCombination
-		data, err := e.fdb.Get("recent")
-		if err != nil {
-			return
-		}
-		err = json.Unmarshal(data, &recents)
-		if err != nil {
-			return
-		}
-		combo := RecentCombination{
-			Recipe: [2]string{elem1, elem2},
-			Result: id,
-		}
-		recents = append([]RecentCombination{combo}, recents...)
-		if len(recents) > recentsLength {
-			recents = recents[:recentsLength-1]
-		}
-		e.fdb.SetData("recent", recents)
-	}()
 
 	res, err := e.db.Query("SELECT COUNT(1) FROM elements WHERE name=?", existing.Name)
 	defer res.Close()
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 
 	parent1, err := e.GetElement(elem1)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 	parent2, err := e.GetElement(elem2)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 	comp1, err := e.calcComplexity(parent1)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 	comp2, err := e.calcComplexity(parent2)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 	complexity := max(comp1, comp2) + 1
 
 	err = e.incrementUses(elem1)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
 	if elem2 != elem1 {
 		err = e.incrementUses(elem2)
 		if err != nil {
-			panic(err)
+			return false, err.Error()
 		}
 	}
 
@@ -150,14 +128,34 @@ func (e *Elemental) CreateSuggestion(mark string, pioneer string, elem1 string, 
 		color := fmt.Sprintf("%s_%f_%f", existing.Color.Base, existing.Color.Saturation, existing.Color.Lightness)
 		_, err = e.db.Exec("INSERT INTO elements VALUES( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )", existing.Name, color, mark, elem1, elem2, existing.Creator, pioneer, int(time.Now().Unix())*1000, complexity, 0, 0)
 		if err != nil {
-			panic(err)
+			return false, err.Error()
 		}
 	}
 
 	// Create combo
 	err = e.addCombo(elem1, elem2, existing.Name)
 	if err != nil {
-		panic(err)
+		return false, err.Error()
 	}
+
+	// New Recent Combo
+	var recents []RecentCombination
+	data, err := e.fdb.Get("recent")
+	if err != nil {
+		return false, err.Error()
+	}
+	err = json.Unmarshal(data, &recents)
+	if err != nil {
+		return false, err.Error()
+	}
+	combo := RecentCombination{
+		Recipe: [2]string{elem1, elem2},
+		Result: id,
+	}
+	recents = append([]RecentCombination{combo}, recents...)
+	if len(recents) > recentsLength {
+		recents = recents[:recentsLength-1]
+	}
+	e.fdb.SetData("recent", recents)
 	return true, ""
 }
