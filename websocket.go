1160eeae9d43827bb97bfece2adcffd5743893f2
Nv7 <junk4Nv7@gmail.com>
2021-03-13 13:35:07 -0800

    Fix header setting

--- websocket.go
+++ websocket.go
@@ -12,7 +12,7 @@ func websockets(app *fiber.App) {
 	app.Use("/ws", func(c *fiber.Ctx) error {
 		fmt.Println(c.Get("Connection"))
 		fmt.Println(c.Get("Upgrade"))
-		c.Set("Connection", "Upgrade")
+		c.Context().Request.Header.Set("Connection", "Upgrade")
 		fmt.Println(c.Get("Connection"))
 		// IsWebSocketUpgrade returns true if the client
 		// requested upgrade to the WebSocket protocol.
