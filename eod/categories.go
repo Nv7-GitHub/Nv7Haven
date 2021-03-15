b638df256e6b29ae1860f90b3f3302d535ac35d9
Nv7 <junk4Nv7@gmail.com>
2021-03-14 17:26:47 -0700

    Try with g

--- eod/categories.go
+++ eod/categories.go
@@ -159,7 +159,7 @@ func (b *EoD) catCmd(category string, m msg, rsp rsp) {
 	}
 	b.newPageSwitcher(pageSwitcher{
 		Kind:       pageSwitchInv,
-		Title:      fmt.Sprintf("%s (%d, %.1f%%)", category, len(out), float32(found)/float32(len(out))*100),
+		Title:      fmt.Sprintf("%s (%d, %.1g%%)", category, len(out), float32(found)/float32(len(out))*100),
 		PageGetter: b.invPageGetter,
 		Items:      out,
 	}, m, rsp)
