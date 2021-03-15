dacee0145135f0544946672617b3ed418b073a9f
Nv7 <junk4Nv7@gmail.com>
2021-03-15 10:04:19 -0700

    Delete commands script

---
+++ eod/deletecommands/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+const (
+	token    = "ODE5MDc2OTIyODY3NzEyMDMx.YEhW1A.iCZTYR_8YH59k7vlYtUM5LZ8Kn8"
+	clientID = "819076922867712031"
+	guild    = "819077688371314718"
+)
+
+func main() {
+	// Discord bot
+	dg, err := discordgo.New("Bot " + token)
+	if err != nil {
+		panic(err)
+	}
+
+	err = dg.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	cmds, err := dg.ApplicationCommands(clientID, guild)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, cmd := range cmds {
+		fmt.Println(cmd.ID)
+		dg.ApplicationCommandDelete(clientID, guild, cmd.ID)
+	}
+
+	dg.Close()
+}
