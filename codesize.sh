106afc83db362a8b1dc6873e3b97f20a7ea676dd
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-04-26 10:49:12 -0700

    Make codesize script better

--- codesize.sh
+++ codesize.sh
@@ -1,4 +1,8 @@
-echo "Code size:"
-find . -type f -name '*.go' -exec du -ch {} + | grep total
-echo "Number of lines"
-find . -type f -name '*.go' | xargs wc -l | grep total
+SIZE="$(find . -type f -name '*.go' -exec du -ch {} + | grep total)"
+LINES="$(find . -type f -name '*.go' | xargs wc -l | grep total)"
+
+SIZE="${SIZE//total/}"
+LINES="${LINES//total/}"
+
+echo "Size: ${SIZE// /}"
+echo "Lines: ${LINES// /}"
