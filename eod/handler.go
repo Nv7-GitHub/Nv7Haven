f3b52f1ca69c7b25fe1bb3c75bf28c5c10a5c4ed
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-12 13:50:16 -0700

    Fix invalid interaction command error

--- eod/handler.go
+++ eod/handler.go
@@ -1,12 +1,16 @@
 package eod
 
 import (
+	"fmt"
+	"os"
+	"reflect"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 const maxComboLength = 20
+const guild = "" // 819077688371314718 for testing
 
 var combs = []string{
 	"\n",
@@ -74,4 +78,84 @@ func (b *EoD) cmdHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		}
 	}
+}
+
+func (b *EoD) initHandlers() {
+	// Debugging
+	var err error
+	datafile, err = os.OpenFile("eodlogs.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+	b.initInfoChoices()
+
+	cmds, err := b.dg.ApplicationCommands(clientID, guild)
+	if err != nil {
+		panic(err)
+	}
+	cms := make(map[string]*discordgo.ApplicationCommand)
+	for _, cmd := range cmds {
+		cms[cmd.Name] = cmd
+	}
+	for _, val := range commands {
+		cmd, exists := cms[val.Name]
+		if !exists || !reflect.DeepEqual(cmd, val) {
+			if val.Name == "elemsort" {
+				val.Options[0].Choices = infoChoices
+			}
+			_, err := b.dg.ApplicationCommandCreate(clientID, guild, val)
+			if err != nil {
+				fmt.Printf("Failed to update command %s\n", val.Name)
+			} else {
+				fmt.Printf("Updated command %s\n", val.Name)
+			}
+		}
+	}
+
+	b.dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		rsp := b.newRespSlash(i)
+		if (i.Data.Name != "suggest") && (i.Data.Name != "mark") && (i.Data.Name != "image") && (i.Data.Name != "inv") && (i.Data.Name != "lb") && (i.Data.Name != "addcat") && (i.Data.Name != "cat") && (i.Data.Name != "hint") && (i.Data.Name != "stats") && (i.Data.Name != "idea") && (i.Data.Name != "about") && (i.Data.Name != "path") && (i.Data.Name != "get") && (i.Data.Name != "rmcat") {
+			isMod, err := b.isMod(i.Member.User.ID, i.GuildID, bot.newMsgSlash(i))
+			if rsp.Error(err) {
+				return
+			}
+			if !isMod {
+				rsp.ErrorMessage("You need to have permission `Administrator`!")
+				return
+			}
+		}
+		if i.Data.Name == "path" {
+			isMod, err := b.isMod(i.Member.User.ID, i.GuildID, bot.newMsgSlash(i))
+			if rsp.Error(err) {
+				return
+			}
+			if !isMod {
+				lock.RLock()
+				dat, exists := b.dat[i.GuildID]
+				lock.RUnlock()
+				if !exists {
+					rsp.ErrorMessage("You need to have permission `Administrator`!")
+					return
+				}
+				inv, exists := dat.invCache[i.Member.User.ID]
+				if !exists {
+					rsp.ErrorMessage("You need to have permission `Administrator`!")
+					return
+				}
+				_, exists = inv[strings.ToLower(i.Data.Options[0].StringValue())]
+				if !exists {
+					rsp.ErrorMessage("You don't have that element!")
+					return
+				}
+			}
+		}
+		if h, ok := commandHandlers[i.Data.Name]; ok {
+			h(s, i)
+		}
+	})
+	b.dg.AddHandler(b.cmdHandler)
+	b.dg.AddHandler(b.reactionHandler)
+	b.dg.AddHandler(b.unReactionHandler)
+	b.dg.AddHandler(b.pageSwitchHandler)
 }
--- eod/init.go
+++ eod/init.go
@@ -3,83 +3,12 @@ package eod
 import (
 	"encoding/json"
 	"fmt"
-	"log"
-	"os"
 	"strings"
 	"time"
-
-	"github.com/bwmarrin/discordgo"
 )
 
 func (b *EoD) init() {
-	// Debugging
-	var err error
-	datafile, err = os.OpenFile("eodlogs.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, os.ModePerm)
-	if err != nil {
-		panic(err)
-	}
-
-	b.initInfoChoices()
-	for _, v := range commands {
-		go func(val *discordgo.ApplicationCommand) {
-			if val.Name == "elemsort" {
-				val.Options[0].Choices = infoChoices
-			}
-			_, err := b.dg.ApplicationCommandCreate(clientID, "", val) // 819077688371314718 for testing
-			if err != nil {
-				fmt.Printf("Failed to update command %s\n", val.Name)
-			}
-		}(v)
-	}
-	b.dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		rsp := b.newRespSlash(i)
-		if (i.Data.Name != "suggest") && (i.Data.Name != "mark") && (i.Data.Name != "image") && (i.Data.Name != "inv") && (i.Data.Name != "lb") && (i.Data.Name != "addcat") && (i.Data.Name != "cat") && (i.Data.Name != "hint") && (i.Data.Name != "stats") && (i.Data.Name != "idea") && (i.Data.Name != "about") && (i.Data.Name != "path") && (i.Data.Name != "get") && (i.Data.Name != "rmcat") {
-			isMod, err := b.isMod(i.Member.User.ID, i.GuildID, bot.newMsgSlash(i))
-			if rsp.Error(err) {
-				return
-			}
-			if !isMod {
-				rsp.ErrorMessage("You need to have permission `Administrator`!")
-				return
-			}
-		}
-		if i.Data.Name == "path" {
-			isMod, err := b.isMod(i.Member.User.ID, i.GuildID, bot.newMsgSlash(i))
-			if rsp.Error(err) {
-				return
-			}
-			if !isMod {
-				lock.RLock()
-				dat, exists := b.dat[i.GuildID]
-				lock.RUnlock()
-				if !exists {
-					rsp.ErrorMessage("You need to have permission `Administrator`!")
-					return
-				}
-				inv, exists := dat.invCache[i.Member.User.ID]
-				if !exists {
-					rsp.ErrorMessage("You need to have permission `Administrator`!")
-					return
-				}
-				_, exists = inv[strings.ToLower(i.Data.Options[0].StringValue())]
-				if !exists {
-					rsp.ErrorMessage("You don't have that element!")
-					return
-				}
-			}
-		}
-		if h, ok := commandHandlers[i.Data.Name]; ok {
-			h(s, i)
-		}
-	})
-	b.dg.AddHandler(b.cmdHandler)
-	b.dg.AddHandler(b.reactionHandler)
-	b.dg.AddHandler(b.unReactionHandler)
-	b.dg.AddHandler(b.pageSwitchHandler)
-	b.dg.AddHandler(func(s *discordgo.Session, i *discordgo.Disconnect) {
-		log.Println("Disconnected!")
-	})
-
+	b.initHandlers()
 	res, err := b.db.Query("SELECT * FROM eod_serverdata WHERE 1")
 	if err != nil {
 		panic(err)
