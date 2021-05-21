994de24842344413011d69113cf988251dfc67c4
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-05-20 18:15:21 -0700

    Collate it

--- eod/other.go
+++ eod/other.go
@@ -69,7 +69,11 @@ func (b *EoD) hintCmd(elem string, hasElem bool, m msg, rsp rsp) {
 
 	var combs *sql.Rows
 	var err error
-	combs, err = b.db.Query("SELECT elems FROM eod_combos WHERE elem3 LIKE ? AND guild=?", elem, m.GuildID)
+	query := "SELECT elems FROM eod_combos WHERE elem3 LIKE ? AND guild=?"
+	if isASCII(elem) {
+		query = "SELECT elems FROM eod_combos WHERE ONVERT(elem3 USING utf8mb4) LIKE CONVERT(? USING utf8mb4) AND guild=CONVERT(? USING utf8mb4) COLLATE utf8mb4_general_ci"
+	}
+	combs, err = b.db.Query(query, elem, m.GuildID)
 	if rsp.Error(err) {
 		return
 	}
