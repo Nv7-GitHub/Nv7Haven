14642f7f854c3dcf7f0803ef4e85b77e5edc8e55
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-06-02 09:03:29 -0700

    Try to support !s

--- eod/handler.go
+++ eod/handler.go
@@ -9,77 +9,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-const maxComboLength = 20
 const guild = "" // 819077688371314718 for testing
-
-var combs = []string{
-	"\n",
-	"+",
-	",",
-}
-
-func (b *EoD) cmdHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-	msg := b.newMsgNormal(m)
-	rsp := b.newRespNormal(m)
-
-	if m.Author.ID == s.State.User.ID || m.Author.Bot {
-		return
-	}
-
-	if strings.HasPrefix(m.Content, "?") {
-		if len(m.Content) < 2 {
-			return
-		}
-		b.infoCmd(strings.TrimSpace(m.Content[1:]), msg, rsp)
-		return
-	}
-
-	if strings.HasPrefix(m.Content, "*2") {
-		if !b.checkServer(msg, rsp) {
-			return
-		}
-		lock.RLock()
-		dat, exists := b.dat[msg.GuildID]
-		lock.RUnlock()
-		if !exists {
-			return
-		}
-		if dat.combCache == nil {
-			dat.combCache = make(map[string]comb)
-		}
-		comb, exists := dat.combCache[msg.Author.ID]
-		if !exists {
-			return
-		}
-		if comb.elem3 != "" {
-			b.combine([]string{comb.elem3, comb.elem3}, msg, rsp)
-			return
-		}
-		b.combine(comb.elems, msg, rsp)
-		return
-	}
-
-	for _, comb := range combs {
-		if strings.Contains(m.Content, comb) {
-			if !b.checkServer(msg, rsp) {
-				return
-			}
-			parts := strings.Split(m.Content, comb)
-			if len(parts) < 2 {
-				return
-			}
-			for i, part := range parts {
-				parts[i] = strings.TrimSpace(strings.Replace(part, "\\", "", -1))
-			}
-			if len(parts) > maxComboLength {
-				rsp.ErrorMessage(fmt.Sprintf("You can only combine up to %d elements!", maxComboLength))
-				return
-			}
-			b.combine(parts, msg, rsp)
-			return
-		}
-	}
-}
 
 func (b *EoD) initHandlers() {
 	// Debugging
