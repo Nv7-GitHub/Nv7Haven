1b8758eb081c0f523611699dd8d0a7e8782dac0b
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-07-05 16:38:10 -0700

    Change build system

--- run.sh
+++ run.sh
@@ -1,3 +1,17 @@
 export PORT=8080
 export PHP_PORT=3000
-docker compose up --build
+export MYSQL_HOST=127.0.0.1
+
+trap "echo \"Gracefully shutting down...\" && sudo -E docker-compose down" EXIT
+
+echo "Building..."
+go build -o main -tags="arm_logs"
+
+echo "Starting..."
+sudo -E docker-compose up -d
+
+echo "Runing..."
+until ./main; do
+  echo "Server 'nv7haven' crashed with exit code $?.  Respawning.." >&2
+  sleep 1
+done
