3e30e99f8026be3ace7225fc24f32f2fed6f3922
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-07-05 16:52:20 -0700

    Remove docker compose commands

--- run.sh
+++ run.sh
@@ -2,13 +2,8 @@ export PORT=8080
 export PHP_PORT=3000
 export MYSQL_HOST=127.0.0.1
 
-trap "echo \"Gracefully shutting down...\" && sudo -E docker-compose down" EXIT
-
 echo "Building..."
 go build -o main -tags="arm_logs"
-
-echo "Starting..."
-sudo -E docker-compose up -d
 
 echo "Running..."
 until ./main; do
