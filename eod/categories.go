f8df4b7fd7ace7fc6bf60fd1e3fad84e5968d04e
sarperen <43860774+sarperen@users.noreply.github.com>
2021-07-07 21:57:39 +0100

    Add autocat all "all" elements

--- eod/categories.go
+++ eod/categories.go
@@ -19,7 +19,8 @@ var autocats = map[string]func(string) bool{
 		_, err := strconv.ParseFloat(s, 32)
 		return err == nil
 	},
-	"Vukkies":    func(s string) bool { return strings.Contains(strings.ToLower(s), "vukky") }
+	"Vukkies":    func(s string) bool { return strings.Contains(strings.ToLower(s), "vukky") },
+	"All \"All\" Elements":     func(s string) bool { return strings.HasPrefix(strings.ToLower(s), "all ") }
 }
 
 func (b *EoD) autocategorize(elem string, guild string) error {
