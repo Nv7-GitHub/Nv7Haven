068e585def3ccc2846bde4a6fabdf9ef59817b09
Nv7-GitHub <junk4Nv7@gmail.com>
2021-07-30 10:53:07 -0700

    Revert "Try auto-select"
    This reverts commit 2625c3187b0715b73af5ede6c5903f48786e5cc5.

--- eod/help.go
+++ eod/help.go
@@ -18,40 +18,40 @@ var helpAdvanced string
 //go:embed help/setup.txt
 var helpSetup string
 
-var helpComponents = discordgo.ActionsRow{
-	Components: []discordgo.MessageComponent{
-		discordgo.SelectMenu{
-			MinValues: 1,
-			MaxValues: 1,
-			CustomID:  "help-select",
-			Options: []discordgo.SelectMenuOption{
-				{
-					Label:       "About",
-					Value:       "about",
-					Description: "Get basic information about the bot!",
-					Default:     true,
-				},
-				{
-					Label:       "Basics",
-					Value:       "basics",
-					Description: "Learn the basics about using the bot!",
-					Default:     false,
-				},
-				{
-					Label:       "Advanced",
-					Value:       "advanced",
-					Description: "Learn how to use the advanced features of the bot!",
-					Default:     false,
-				},
-				{
-					Label:       "Setup",
-					Value:       "setup",
-					Description: "Learn how to set up your own EoD server!",
-					Default:     false,
+func makeHelpComponents(selected string) discordgo.ActionsRow {
+	return discordgo.ActionsRow{
+		Components: []discordgo.MessageComponent{
+			discordgo.SelectMenu{
+				CustomID: "help-select",
+				Options: []discordgo.SelectMenuOption{
+					{
+						Label:       "About",
+						Value:       "about",
+						Description: "Get basic information about the bot!",
+						Default:     selected == "about",
+					},
+					{
+						Label:       "Basics",
+						Value:       "basics",
+						Description: "Learn the basics about using the bot!",
+						Default:     selected == "basics",
+					},
+					{
+						Label:       "Advanced",
+						Value:       "advanced",
+						Description: "Learn how to use the advanced features of the bot!",
+						Default:     selected == "advanced",
+					},
+					{
+						Label:       "Setup",
+						Value:       "setup",
+						Description: "Learn how to set up your own EoD server!",
+						Default:     selected == "setup",
+					},
 				},
 			},
 		},
-	},
+	}
 }
 
 type helpComponent struct {
@@ -61,7 +61,8 @@ type helpComponent struct {
 func (h *helpComponent) handler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	var txt string
 
-	switch i.MessageComponentData().Values[0] {
+	val := i.MessageComponentData().Values[0]
+	switch val {
 	case "about":
 		txt = helpAbout
 	case "basics":
@@ -78,14 +79,14 @@ func (h *helpComponent) handler(s *discordgo.Session, i *discordgo.InteractionCr
 		Type: discordgo.InteractionResponseUpdateMessage,
 		Data: &discordgo.InteractionResponseData{
 			Content:    txt,
-			Components: []discordgo.MessageComponent{helpComponents},
+			Components: []discordgo.MessageComponent{makeHelpComponents(val)},
 		},
 	})
 }
 
 func (b *EoD) helpCmd(m msg, rsp rsp) {
 	rsp.Acknowledge()
-	id := rsp.Message(helpAbout, helpComponents)
+	id := rsp.Message(helpAbout, makeHelpComponents("about"))
 
 	lock.RLock()
 	dat, exists := b.dat[m.GuildID]
