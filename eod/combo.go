50e50e17fd27ed66069d533adb435a3d556b6e19
Nv7 <junk4Nv7@gmail.com>
2021-08-18 09:56:21 -0700

    Fix error messages

--- eod/combo.go
+++ eod/combo.go
@@ -37,6 +37,7 @@ func (b *EoD) combine(elems []string, m types.Msg, rsp types.Rsp) {
 		return
 	}
 
+	// Get rid of nulls
 	validElems := make([]string, len(elems))
 	validCnt := 0
 	for _, elem := range elems {
@@ -51,56 +52,75 @@ func (b *EoD) combine(elems []string, m types.Msg, rsp types.Rsp) {
 		return
 	}
 
+	// Check if don't have or not exists
+	donthave := false
+	elExists := true
 	for _, elem := range elems {
 		_, res := dat.GetElement(elem)
 		if !res.Exists {
-			notExists := make(map[string]types.Empty)
-			for _, el := range elems {
-				_, res = dat.GetElement(el)
-				if !res.Exists {
-					notExists["**"+el+"**"] = types.Empty{}
-				}
-			}
-			if len(notExists) == 1 {
-				rsp.ErrorMessage(fmt.Sprintf("Element **%s** doesn't exist!", elem))
-				return
-			}
-
-			rsp.ErrorMessage("Elements " + joinTxt(notExists, "and") + " don't exist!")
-			return
+			elExists = false
 		}
 
 		_, hasElement := inv[strings.ToLower(elem)]
 		if !hasElement {
-			_, res := dat.GetComb(m.Author.ID)
-			if res.Exists {
-				dat.DeleteComb(m.Author.ID)
-
-				lock.Lock()
-				b.dat[m.GuildID] = dat
-				lock.Unlock()
-			}
-
-			notFound := make(map[string]types.Empty)
-			for _, el := range elems {
-				_, exists := inv[strings.ToLower(el)]
-				if !exists {
-					elem, _ := dat.GetElement(el)
-					notFound["**"+elem.Name+"**"] = types.Empty{}
-				}
-			}
-
-			if len(notFound) == 1 {
-				el, _ := dat.GetElement(elem)
-				rsp.ErrorMessage(fmt.Sprintf("You don't have **%s**!", el.Name))
-				return
-			}
-
-			rsp.ErrorMessage("You don't have " + joinTxt(notFound, "or") + "!")
-			return
+			donthave = true
 		}
 	}
+	if !elExists {
+		notExists := make(map[string]types.Empty)
+		for _, el := range elems {
+			_, res = dat.GetElement(el)
+			if !res.Exists {
+				notExists["**"+el+"**"] = types.Empty{}
+			}
+		}
+		if len(notExists) == 1 {
+			el := ""
+			for k := range notExists {
+				el = k
+				break
+			}
+			rsp.ErrorMessage(fmt.Sprintf("Element **%s** doesn't exist!", el))
+			return
+		}
 
+		rsp.ErrorMessage("Elements " + joinTxt(notExists, "and") + " don't exist!")
+		return
+	}
+	if donthave {
+		_, res := dat.GetComb(m.Author.ID)
+		if res.Exists {
+			dat.DeleteComb(m.Author.ID)
+
+			lock.Lock()
+			b.dat[m.GuildID] = dat
+			lock.Unlock()
+		}
+
+		notFound := make(map[string]types.Empty)
+		for _, el := range elems {
+			_, exists := inv[strings.ToLower(el)]
+			if !exists {
+				elem, _ := dat.GetElement(el)
+				notFound["**"+elem.Name+"**"] = types.Empty{}
+			}
+		}
+
+		if len(notFound) == 1 {
+			el := ""
+			for k := range notFound {
+				el = k
+				break
+			}
+			rsp.ErrorMessage(fmt.Sprintf("You don't have **%s**!", el))
+			return
+		}
+
+		rsp.ErrorMessage("You don't have " + joinTxt(notFound, "or") + "!")
+		return
+	}
+
+	// Combine elements
 	var elem3 string
 	cont := true
 	query := "SELECT elem3 FROM eod_combos WHERE elems LIKE ? AND guild=?"
