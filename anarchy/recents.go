cb46cf892d85f037e174a1dc371f5d3b24be49ee
Nv7 <junk4Nv7@gmail.com>
2021-09-04 20:04:09 -0700

    Recent additions

---
+++ anarchy/recents.go
@@ -0,0 +1,46 @@
+package anarchy
+
+import (
+	"context"
+
+	"github.com/Nv7-Github/Nv7Haven/pb"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+const recentsLength = 30
+
+func (a *Anarchy) GetRecents(ctx context.Context, _ *emptypb.Empty) (*pb.AnarchyRecents, error) {
+	recents := make([]*pb.AnarchyRecentCombination, 0, recentsLength)
+	res, err := a.db.Query("SELECT elem1, elem2, elem3 FROM recents ORDER BY createdon DESC LIMIT ?", recentsLength)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Close()
+
+	for res.Next() {
+		item := &pb.AnarchyRecentCombination{}
+		err = res.Scan(&item.Elem1, &item.Elem2, &item.Elem3)
+		if err != nil {
+			return nil, err
+		}
+
+		recents = append(recents, item)
+	}
+
+	return &pb.AnarchyRecents{
+		Recents: recents,
+	}, nil
+}
+
+func (a *Anarchy) WaitForNextRecent(_ *emptypb.Empty, stream pb.Anarchy_WaitForNextRecentServer) error {
+	a.recents.L.Lock()
+	a.recents.Wait()
+	a.recents.L.Unlock()
+
+	err := stream.Send(&emptypb.Empty{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
