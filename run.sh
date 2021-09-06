992ca5ced64b1ca4519a00d3093723cc2220ed2b
Nv7 <junk4Nv7@gmail.com>
2021-09-06 13:23:02 -0700

    Optimize size

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ export PHP_PORT=3000
 export MYSQL_HOST=127.0.0.1
 
 echo "Building..."
-go build -o main -tags="arm_logs"
+go build -o main -tags="arm_logs" -ldflags="-s -w"
 
 echo "Running..."
 until ./main; do
