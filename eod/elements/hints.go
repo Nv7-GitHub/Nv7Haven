731f39625ee040caf0b35ba186d08a4172ef6f68
Nv7 <junk4Nv7@gmail.com>
2021-12-12 10:00:26 -0800

    More prints

--- eod/elements/hints.go
+++ eod/elements/hints.go
@@ -159,13 +159,14 @@ func (b *Elements) getHint(elem int, db *eodb.DB, hasElem bool, author string, g
 		db.RLock()
 		for elems := range db.Combos() {
 			parts := strings.Split(elems, "+")
-			fmt.Println(len(parts))
 			for _, part := range parts {
 				num, err := strconv.Atoi(part)
 				if err != nil {
+					fmt.Println(err)
 					continue
 				}
 				if num == el.ID {
+					fmt.Println(num)
 					el, _ := db.GetElement(num)
 					vals[el.Name] = types.Empty{}
 					break
