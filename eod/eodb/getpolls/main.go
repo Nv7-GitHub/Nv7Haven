31a4bd74e00e7ad0b041068034d49afb8f497836
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-12-18 14:16:21 -0800

    Timing

--- eod/eodb/getpolls/main.go
+++ eod/eodb/getpolls/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/Nv7-Github/Nv7Haven/eod/eodb"
 )
@@ -18,8 +19,11 @@ func main() {
 	home, err := os.UserHomeDir()
 	handle(err)
 	dbPath := filepath.Join(home, "go/src/github.com/Nv7-Github/Nv7haven/data/eod")
+	fmt.Println("Loading...")
+	start := time.Now()
 	db, err := eodb.NewData(dbPath)
 	handle(err)
+	fmt.Println("Loaded in", time.Since(start))
 
 	// Get polls
 	for _, db := range db.DB {
