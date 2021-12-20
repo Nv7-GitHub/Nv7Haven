f7413fe9f3ab507496cb90600ce0159269fe64d8
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-12-20 10:45:53 -0800

    Get rid of download_eodb, new backup fixes

--- nv7haven/nv7haven.go
+++ nv7haven/nv7haven.go
@@ -54,7 +54,6 @@ func (c *Nv7Haven) routing(app *fiber.App) {
 	app.Post("/http_get", c.httpGet)
 	app.Get("/eod_stats", c.getEodStats)
 	app.Get("/name_count/:name", c.nameCount)
-	app.Post("/download_eodb", c.downloadEoDDB)
 }
 
 // InitNv7Haven initializes the handlers for Nv7Haven
