8182c87a892a1296c83bb4d60e2cc57b670be83b
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2021-12-29 00:59:26 -0800

    Update backup script

--- scripts/gitbackup.sh
+++ scripts/gitbackup.sh
@@ -1,5 +1,5 @@
 echo "Backing up..."
-cd ~/go/src/github.com/Nv7-Github/Nv7haven/data/eod
+cd ~/go/src/github.com/Nv7-Github/Nv7haven/data
 git add -A
 git commit -m "Backup $(date +%m/%d/%Y)"
 git push origin main
