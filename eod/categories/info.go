fb35b80842cf8f27925c4e9c43fbbe4e4197d62f
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-05-30 22:27:49 -0700

    Bugfix

--- eod/categories/info.go
+++ eod/categories/info.go
@@ -19,18 +19,23 @@ func (b *Categories) InfoCmd(catName string, m types.Msg, rsp types.Rsp) {
 	cat, exists := db.GetCat(catName)
 	if exists.Exists {
 		// Cat info
-		rsp.Embed(&discordgo.MessageEmbed{
+		emb := &discordgo.MessageEmbed{
 			Title: fmt.Sprintf("%s Info", cat.Name),
 			Thumbnail: &discordgo.MessageEmbedThumbnail{
 				URL: cat.Image,
 			},
 			Fields: []*discordgo.MessageEmbedField{
 				{Name: db.Config.LangProperty("ElementCount", nil), Value: strconv.Itoa(len(cat.Elements))},
-				{Name: db.Config.LangProperty("InfoImager", nil), Value: fmt.Sprintf("<@%s>", cat.Imager)},
-				{Name: db.Config.LangProperty("InfoColorer", nil), Value: fmt.Sprintf("<@%s>", cat.Colorer)},
 			},
 			Color: cat.Color,
-		})
+		}
+		if cat.Imager != "" {
+			emb.Fields = append(emb.Fields, &discordgo.MessageEmbedField{Name: db.Config.LangProperty("InfoImager", nil), Value: fmt.Sprintf("<@%s>", cat.Imager)})
+		}
+		if cat.Colorer != "" {
+			emb.Fields = append(emb.Fields, &discordgo.MessageEmbedField{Name: db.Config.LangProperty("InfoColorer", nil), Value: fmt.Sprintf("<@%s>", cat.Colorer)})
+		}
+		rsp.Embed(emb)
 		return
 	}
 
@@ -53,14 +58,18 @@ func (b *Categories) InfoCmd(catName string, m types.Msg, rsp types.Rsp) {
 		},
 		Fields: []*discordgo.MessageEmbedField{
 			{Name: db.Config.LangProperty("ElementCount", nil), Value: strconv.Itoa(len(els)), Inline: true},
-			{Name: db.Config.LangProperty("InfoImager", nil), Value: fmt.Sprintf("<@%s>", vcat.Imager), Inline: true},
-			{Name: db.Config.LangProperty("InfoColorer", nil), Value: fmt.Sprintf("<@%s>", vcat.Colorer), Inline: true},
-			{Name: "Kind", Value: vcat.Rule.String(), Inline: true}, // TODO: Translate
 		},
 		Color: vcat.Color,
 	}
+	if vcat.Imager != "" {
+		emb.Fields = append(emb.Fields, &discordgo.MessageEmbedField{Name: db.Config.LangProperty("InfoImager", nil), Value: fmt.Sprintf("<@%s>", vcat.Imager), Inline: true})
+	}
+	if vcat.Colorer != "" {
+		emb.Fields = append(emb.Fields, &discordgo.MessageEmbedField{Name: db.Config.LangProperty("InfoColorer", nil), Value: fmt.Sprintf("<@%s>", vcat.Colorer), Inline: true})
+	}
 
 	// TODO: Translate fields below
+	emb.Fields = append(emb.Fields, &discordgo.MessageEmbedField{Name: "Kind", Value: vcat.Rule.String(), Inline: true}) // TODO: Translate)
 	switch vcat.Rule {
 	case types.VirtualCategoryRuleRegex:
 		emb.Fields = append(emb.Fields, &discordgo.MessageEmbedField{Name: "Regex", Value: "`" + vcat.Data["regex"].(string) + "`", Inline: true})
