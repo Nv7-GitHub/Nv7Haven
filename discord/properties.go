f1d3befdb861e276fd88e9a21e332bed540bf44e
Ima Person <me@imaperson.dev>
2022-07-03 11:37:39 -0400

    I don't understand Go
    cannot convert float32(300 * 100) * float32(1) / 3600 (constant x of type float32) to type int

--- discord/properties.go
+++ discord/properties.go
@@ -235,8 +235,11 @@ func (b *Bot) properties(s *discordgo.Session, m *discordgo.MessageCreate) {
 		for id, ups := range user.Properties {
 			val := b.props[id].Value
 
-			income += int(float32(val*ups) * float32(1) / 3600)
-			limbo += int(float32(val*ups) * (float32(time.Now().Unix()-user.LastVisited) / 3600))
+			onesec := float32(val*ups) * float32(1) / 3600
+			moneyEarned := float32(val*ups) * (float32(time.Now().Unix()-user.LastVisited) / 3600)
+
+			income += int(onesec)
+			limbo += int(moneyEarned)
 		}
 		if id == m.Author.ID {
 			s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Your current income is %d coins, and you have %d in limbo!", income, limbo))
