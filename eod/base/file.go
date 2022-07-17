bf8c5f6630310b93851d48beca401bab17b0213a
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-07-16 17:24:08 -0700

    Fix gzip

--- eod/base/file.go
+++ eod/base/file.go
@@ -1,7 +1,9 @@
 package base
 
 import (
+	"bytes"
 	"compress/gzip"
+	"io"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -10,13 +12,20 @@ const maxLen = 8000000
 
 func (b *Base) PrepareFile(d *discordgo.File, len int) *discordgo.File {
 	if len > maxLen {
+		buf := bytes.NewBuffer(nil)
+		gz := gzip.NewWriter(buf)
+		_, err := io.Copy(gz, d.Reader)
+		if err != nil {
+			return d
+		}
+		err = gz.Close()
+		if err != nil {
+			return d
+		}
+
 		d.ContentType = "application/gzip"
 		d.Name += ".gz"
-		var err error
-		d.Reader, err = gzip.NewReader(d.Reader)
-		if err != nil {
-			panic(err)
-		}
+		d.Reader = buf
 	}
 	return d
 }
