3029c49cbca45b3f47c14bc095ae2f7230d25615
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-08-15 21:00:00 -0700

    Update sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,9 @@
 echo "Removing..."
 rm -rf "$(go env GOPATH)/src/github.com/Nv7-Github/Nv7haven/data"
-echo "Copying..."
-scp -P 119 -r pi@98.247.143.47:/home/pi/go/src/github.com/Nv7-Github/Nv7haven/data "$(go env GOPATH)/src/github.com/Nv7-Github/Nv7haven/data" 
+echo "Downloading..."
+curl "https://nv7haven.nv7haven.com/sync_db/$PASSWORD" > sync.zip
+echo "Unzipping..."
+unzip sync.zip -d "$(go env GOPATH)/src/github.com/Nv7-Github/Nv7haven"
+echo "Cleaning up..."
+rm sync.zip
+echo "Done!"
