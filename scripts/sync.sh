a404f449645df726fbacc3a660e66b8d591a9d1c
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-08-31 04:04:14 +0000

    Make it work on codespaces

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,9 +1,9 @@
 echo "Removing..."
-rm -rf "$(go env GOPATH)/src/github.com/Nv7-Github/Nv7haven/data"
+rm -rf "data"
 echo "Downloading..."
 curl "https://nv7haven.nv7haven.com/sync_db/$PASSWORD" > sync.zip
 echo "Unzipping..."
-unzip sync.zip -d "$(go env GOPATH)/src/github.com/Nv7-Github/Nv7haven"
+unzip sync.zip -d .
 echo "Cleaning up..."
 rm sync.zip
 echo "Done!"
