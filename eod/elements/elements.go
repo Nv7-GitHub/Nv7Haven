1ee578a52154e55cacf89dda3a26050d45a55a09
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-12-29 19:56:20 -0800

    Edit command

--- eod/elements/elements.go
+++ eod/elements/elements.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Nv7-Github/Nv7Haven/eod/base"
 	"github.com/Nv7-Github/Nv7Haven/eod/polls"
 	"github.com/Nv7-Github/sevcord/v2"
+	"github.com/bwmarrin/discordgo"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -43,6 +44,51 @@ func (e *Elements) Init() {
 		sevcord.NewOption("element", "The element to view the path of!", sevcord.OptionKindInt, true).
 			AutoComplete(e.Autocomplete),
 	))
+	e.s.RegisterSlashCommand(sevcord.NewSlashCommandGroup("edit", "Edit element properties!",
+		sevcord.NewSlashCommand("name",
+			"Edit the name of an element!",
+			e.EditElementNameCmd,
+			sevcord.NewOption("element", "The element to edit!", sevcord.OptionKindInt, true).
+				AutoComplete(e.Autocomplete),
+			sevcord.NewOption("name", "The new name of the element!", sevcord.OptionKindString, true),
+		),
+		sevcord.NewSlashCommand("image",
+			"Edit the image of an element!",
+			e.EditElementImageCmd,
+			sevcord.NewOption("element", "The element to edit!", sevcord.OptionKindInt, true).
+				AutoComplete(e.Autocomplete),
+			sevcord.NewOption("image", "The new image URL of the element!", sevcord.OptionKindString, true),
+		),
+		sevcord.NewSlashCommand("description",
+			"Edit the description of an element!",
+			e.EditElementCommentCmd,
+			sevcord.NewOption("element", "The element to edit!", sevcord.OptionKindInt, true).
+				AutoComplete(e.Autocomplete),
+			sevcord.NewOption("description", "The new description of the element!", sevcord.OptionKindString, true),
+		),
+		sevcord.NewSlashCommand("color",
+			"Edit the color of an element!",
+			e.EditElementColorCmd,
+			sevcord.NewOption("element", "The element to edit!", sevcord.OptionKindInt, true).
+				AutoComplete(e.Autocomplete),
+			sevcord.NewOption("color", "The new color of the element! (decimal version of hex code)", sevcord.OptionKindInt, true),
+		),
+		sevcord.NewSlashCommand("creator",
+			"Edit the creator of an element!",
+			e.EditElementCreatorCmd,
+			sevcord.NewOption("element", "The element to edit!", sevcord.OptionKindInt, true).
+				AutoComplete(e.Autocomplete),
+			sevcord.NewOption("creator", "The new creator of the element!", sevcord.OptionKindUser, true),
+		),
+		sevcord.NewSlashCommand("createdon",
+			"Edit the creation date of an element!",
+			e.EditElementCreatedonCmd,
+			sevcord.NewOption("element", "The element to edit!", sevcord.OptionKindInt, true).
+				AutoComplete(e.Autocomplete),
+			sevcord.NewOption("createdon", "The new creation date of the element! (unix timestamp)", sevcord.OptionKindInt, true),
+		),
+	).
+		RequirePermissions(discordgo.PermissionManageServer))
 }
 
 func NewElements(s *sevcord.Sevcord, db *sqlx.DB, base *base.Base, polls *polls.Polls) *Elements {
