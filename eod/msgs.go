fa0531eb361d95659b5e0ed7e529cb6d27edafbc
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-12-31 20:41:13 -0800

    Add text commands

--- eod/msgs.go
+++ eod/msgs.go
@@ -13,7 +13,53 @@ var seps = []string{
 	"plus",
 }
 
+func (b *Bot) getElementId(c sevcord.Ctx, val string) (int64, bool) {
+	var id int64
+	err := b.db.QueryRow("SELECT id FROM elements WHERE LOWER(name)=$1 AND guild=$2", strings.ToLower(strings.TrimSpace(val)), c.Guild()).Scan(&id)
+	if err != nil {
+		b.base.Error(c, err)
+		return 0, false
+	}
+	return id, true
+}
+
+func (b *Bot) textCommandHandler(c sevcord.Ctx, name string, content string) {
+	switch name {
+	case "s", "suggest":
+		b.base.IncrementCommandStat(c, "suggest")
+		b.elements.Suggest(c, []any{any(content), nil})
+
+	case "h", "hint":
+		b.base.IncrementCommandStat(c, "hint")
+		val := any(nil)
+		if content != "" {
+			v, ok := b.getElementId(c, content)
+			if !ok {
+				return
+			}
+			val = any(v)
+		}
+		b.elements.Hint(c, []any{val, nil})
+
+	case "cat":
+		b.base.IncrementCommandStat(c, "cat")
+		b.pages.Cat(c, []any{any(content), nil})
+
+	case "p", "products":
+		b.base.IncrementCommandStat(c, "products")
+		id, ok := b.getElementId(c, content)
+		if !ok {
+			return
+		}
+		b.elements.Products(c, []any{any(id), nil})
+	}
+}
+
 func (b *Bot) messageHandler(c sevcord.Ctx, content string) {
+	if strings.HasPrefix(content, "!") {
+		parts := strings.SplitN(content[1:], " ", 2)
+		b.textCommandHandler(c, parts[0], parts[1])
+	}
 	if strings.HasPrefix(content, "?") {
 		b.elements.InfoMsgCmd(c, content[1:])
 		return
