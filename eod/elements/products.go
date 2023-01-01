86e79c771abaa561f020a836f3377e890f9b3ac9
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-12-31 19:28:47 -0800

    Print error

--- eod/elements/products.go
+++ eod/elements/products.go
@@ -63,5 +63,8 @@ func (e *Elements) Products(c sevcord.Ctx, opts []any) {
 		Description(desc.String()).
 		Footer(fmt.Sprintf("%s Products", humanize.Comma(int64(len(items)))), "").
 		Color(15548997) // Fuschia
-	c.Respond(sevcord.NewMessage("").AddEmbed(emb))
+	err = c.Respond(sevcord.NewMessage("").AddEmbed(emb))
+	if err != nil {
+		fmt.Println("products respond err", err)
+	}
 }
