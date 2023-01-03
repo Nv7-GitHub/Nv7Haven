fd1d4df5b44ce23835b4b76ea219930aa6af4adf
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-01-02 16:54:49 -0800

    Bugfix

--- eod/queries/info.go
+++ eod/queries/info.go
@@ -90,7 +90,7 @@ func (q *Queries) Info(ctx sevcord.Ctx, opts []any) {
 		emb = emb.AddField("Kind", "Comparison", true)
 		emb = emb.AddField("Field", "`"+qu.Data["field"].(string)+"`", true)
 		emb = emb.AddField("Operator", qu.Data["typ"].(string), true)
-		emb = emb.AddField("Value", fmt.Sprintf("%v", qu.Data["val"]), true)
+		emb = emb.AddField("Value", fmt.Sprintf("%v", qu.Data["value"]), true)
 	}
 
 	// Respond
