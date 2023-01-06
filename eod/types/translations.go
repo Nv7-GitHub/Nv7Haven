dfbc3b48d65257800ee4c317aa2ddca81551d91a
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2023-01-06 07:06:18 -0500

    Update translations.go

--- eod/types/translations.go
+++ eod/types/translations.go
@@ -1,9 +1,6 @@
 package types
 
 import (
-	"bytes"
-	"fmt"
-	"reflect"
 	"text/template"
 )
 
@@ -33,7 +30,7 @@ var LanguageTable = []Language{
 		translations: translations{
 			`A name may not contain '{{.var1}}'!`,
 			"Name cannot be empty!",
-			"Successfully updated {{.var1}}!",
+			`Successfully updated {{.var1}}!`,
 			"**PLAY CHANNELS HAVE BEEN RESET**\nUpdate them below!",
 			"You are not authorized to use this!",
 			"Successfully updated play channels!",
