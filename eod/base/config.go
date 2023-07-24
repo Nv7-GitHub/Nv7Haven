fa66167d59ceb3767ba1ff6f2dd21147a505a3cb
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-07-24 10:59:36 -0700

    Add config to news

--- eod/base/config.go
+++ eod/base/config.go
@@ -2,11 +2,26 @@ package base
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/Nv7-Github/sevcord/v2"
 	"github.com/bwmarrin/discordgo"
 	"github.com/lib/pq"
 )
+
+func (b *Base) configNewsMessage(c sevcord.Ctx, message string) {
+	var news string
+	err := b.db.QueryRow(`SELECT news FROM config WHERE guild=$1`, c.Guild()).Scan(&news)
+	if err != nil {
+		log.Println("news err", err)
+		return
+	}
+	_, err = c.Dg().ChannelMessageSend(news, fmt.Sprintf("⚙ "+message))
+	if err != nil {
+		log.Println("news err", err)
+	}
+}
 
 func (b *Base) configChannel(c sevcord.Ctx, field string, value any, message string) {
 	c.Acknowledge()
@@ -18,6 +33,7 @@ func (b *Base) configChannel(c sevcord.Ctx, field string, value any, message str
 	}
 
 	c.Respond(sevcord.NewMessage(fmt.Sprintf("Successfully updated %s!", message)))
+	b.configNewsMessage(c, fmt.Sprintf("Changed Config - %s", strings.ToTitle(message)))
 }
 
 func (b *Base) ConfigVoting(c sevcord.Ctx, opts []any) {
@@ -51,6 +67,7 @@ func (b *Base) ConfigPlayChannels(c sevcord.Ctx, opts []any) {
 			ChannelMenuFilter(discordgo.ChannelTypeGuildText).
 			SetRange(0, 25),
 	))
+	b.configNewsMessage(c, "Changed Config - Play Channels")
 }
 
 func (b *Base) ConfigPlayChannelsHandler(c sevcord.Ctx, params string, opts []string) {
