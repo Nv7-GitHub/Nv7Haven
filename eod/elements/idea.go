be1d20e8eba670fbc69a7bd1281ac94acb5f5f5e
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-07-27 15:58:37 -0700

    Bugfix

--- eod/elements/idea.go
+++ eod/elements/idea.go
@@ -31,13 +31,13 @@ func (e *Elements) IdeaHandler(c sevcord.Ctx, params string) {
 		// Get elements
 		var err error
 		if parts[1] == "" {
-			err = e.db.Select(&els, `SELECT id FROM elements WHERE guild=$1 ORDER BY RANDOM() LIMIT 2`, c.Guild())
+			err = e.db.Select(&els, `SELECT id FROM elements WHERE guild=$1 ORDER BY RANDOM() LIMIT $2`, c.Guild(), cnt)
 		} else {
 			query, ok := e.base.CalcQuery(c, parts[1]) // TODO: Move this out of the loop
 			if !ok {
 				return
 			}
-			err = e.db.Select(&els, `SELECT id FROM elements WHERE guild=$1 AND id=ANY($2) ORDER BY RANDOM() LIMIT 2`, c.Guild(), pq.Array(query.Elements))
+			err = e.db.Select(&els, `SELECT id FROM elements WHERE guild=$1 AND id=ANY($2) ORDER BY RANDOM() LIMIT $3`, c.Guild(), pq.Array(query.Elements), cnt)
 		}
 		if err != nil {
 			e.base.Error(c, err)
