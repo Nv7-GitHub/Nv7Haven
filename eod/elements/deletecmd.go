804e303580a808d353632b5ba8f9f32f47b1e4db
rayzchen <tankimarshal2@gmail.com>
2023-08-31 16:55:42 +0000

    Fix comments and unused code in combo deletion

--- eod/elements/deletecmd.go
+++ eod/elements/deletecmd.go
@@ -29,16 +29,7 @@ func (e *Elements) DeleteComboCmd(c sevcord.Ctx, opts []any) {
 		return
 	}
 
-
-	// Get tree size
-	var currtreesize int
-	err = e.db.QueryRow(`SELECT treesize FROM elements WHERE id=$1 AND guild=$2`, opts[0].(int64), c.Guild()).Scan(&currtreesize)
-	if err != nil {
-		e.base.Error(c, err)
-		return
-	}
-
-	// Get tree size
+	// Get remaining combo elements
 	var els pq.Int64Array
 	err = e.db.QueryRow(`SELECT els FROM combos WHERE guild=$2 AND result=$1`, opts[0].(int64), c.Guild()).Scan(&els)
 	if err != nil {
@@ -46,22 +37,19 @@ func (e *Elements) DeleteComboCmd(c sevcord.Ctx, opts []any) {
 		return
 	}
 
-	// Check if need to update parents
+	// Get new tree size
 	var treesize int
-	var loop bool
 	err = e.db.QueryRow(`WITH RECURSIVE parents(els, id) AS (
 		VALUES($2::integer[], 0)
 	UNION
 		(SELECT b.parents els, b.id id FROM elements b INNER JOIN parents p ON b.id=ANY(p.els) where guild=$1)
-	) SELECT COUNT(*), EXISTS(SELECT 1 FROM parents WHERE id=$3) FROM parents WHERE id>0`, c.Guild(), pq.Array(els), opts[0].(int64)).Scan(&treesize, &loop)
-	// Force update since only one combo left
-	if !loop {
-		// Update parents
-		_, err = e.db.Exec(`UPDATE elements SET parents=$1, treesize=$2 WHERE id=$3 AND guild=$4`, pq.Array(els), treesize, opts[0].(int64), c.Guild())
-		if err != nil {
-			e.base.Error(c, err)
-			return
-		}
+	) SELECT COUNT(*)  FROM parents WHERE id>0`, c.Guild(), pq.Array(els), opts[0].(int64)).Scan(&treesize, &loop)
+
+	// Update parents
+	_, err = e.db.Exec(`UPDATE elements SET parents=$1, treesize=$2 WHERE id=$3 AND guild=$4`, pq.Array(els), treesize, opts[0].(int64), c.Guild())
+	if err != nil {
+		e.base.Error(c, err)
+		return
 	}
 
 	// Get element name
@@ -70,6 +58,7 @@ func (e *Elements) DeleteComboCmd(c sevcord.Ctx, opts []any) {
 		e.base.Error(c, err)
 		return
 	}
+	// Send message in news
 	c.Respond(sevcord.NewMessage("Successfully deleted all extra combos! ✅"))
 	e.deleteNewsMessage(c, fmt.Sprintf("Deleted Combos - **%s**", nameE))
 }
