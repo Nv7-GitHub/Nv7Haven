062d726c0599f8c989b8200c305b16bf9dda5967
Nv7-Github <junk4Nv7@gmail.com>
2024-07-28 14:45:35 -0700

    Fix output logs

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ echo "Building..."
 go build -o main -tags="arm_logs" -ldflags="-s -w"
 
 echo "Running..."
-until ./main > output.log; do
+until ./main > output.log 2>&1; do
   echo "Go crashed with exit code $?.  Respawning.." >&2
   sleep 1
 done
