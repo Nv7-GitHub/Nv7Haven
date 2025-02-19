2d894f9edb36e1a3f1be7a79b4eec6c7092869d2
DeviantEquinox <93010117+DeviantEquinox@users.noreply.github.com>
2025-02-19 14:39:28 -0800

    Update sorts.go

--- eod/sorts.go
+++ eod/sorts.go
@@ -190,7 +190,7 @@ func getSort(input string) string {
 }
 func getLbSort(input string) string {
 	switch input {
-	case "made":
+	case "invented":
 		return "made"
 	case "imaged", "img":
 		return "img"
